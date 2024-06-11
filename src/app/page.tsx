467c643148ba0516f2cdcf510d18d4d99acd4312
lxndrbukin <lxndrbukin@gmail.com>
2024-06-11 22:00:44 +0100

    Update app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import styles from "./assets/home.module.scss";
+import CoinsList from "./coins/coins-list";
+
 export default function Home(): JSX.Element {
-  return <main></main>;
+  return (
+    <section className="section">
+      <div className="table-container">
+        <CoinsList />
+      </div>
+    </section>
+  );
 }
